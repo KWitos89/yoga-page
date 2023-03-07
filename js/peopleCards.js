5e6aaa2209e34405bf7873bf73cec2907d44b3ef
Krzysztof Witos <kwitos89@gmail.com>
2023-03-07 20:45:23 +0100

    js,css

--- js/peopleCards.js
+++ js/peopleCards.js
@@ -1,7 +1,7 @@
 const cardOne = document.querySelector('.people__cards--one');
 const cardTwo = document.querySelector('.people__cards--two');
 const cardThree = document.querySelector('.people__cards--three');
-// export const allCards = document.querySelectorAll('.people__cards');
+export const allCards = document.querySelectorAll('.people__cards');
 
 const dotOne = document.querySelector('.dot--one');
 const dotTwo = document.querySelector('.dot--two');
@@ -13,59 +13,44 @@ const box = document.querySelector('.people__box-slider')
 
 
 let width = 102;
+let move = 1;
 let index = 1;
-
-
 const changeCardOne = () => {
     dotOne.classList.add('dot--active')
     dotTwo.classList.remove('dot--active')
     dotThree.classList.remove('dot--active')
 
-    sliderBox.style.transform = `translateX(${width}%)`
-
-
-    if (window.innerWidth <= 768) {
-        let width = 102;
-
-        sliderBox.style.transform = `translateX(${width}%)`
-    }
-
+    sliderBox.style.transform = `translateX(${width}%)`;
+    cardOne.style.transform = `translateX(${-move}%)`;
+    cardOne.style.pointerEvents = "none";
+    cardTwo.style.pointerEvents = "";
 };
 const changeCardTwo = () => {
 
-    dotOne.classList.remove('dot--active')
-    dotTwo.classList.add('dot--active')
-    dotThree.classList.remove('dot--active')
+    dotOne.classList.remove('dot--active');
+    dotTwo.classList.add('dot--active');
+    dotThree.classList.remove('dot--active');
 
     if (sliderBox.style.transform = `translateX(${-width}%)`) {
         sliderBox.style.transform = `translateX(${index / width}%)`
-    }
+        cardOne.style.transform = "";
+        };
+        cardOne.style.pointerEvents = "";
+        cardTwo.style.pointerEvents = "none";
+        cardThree.style.pointerEvents = "none";
 
-    if (window.innerWidth <= 375) {
-        let width = 102;
-
-        if (sliderBox.style.transform = `translateX(${-width}%)`) {
-            sliderBox.style.transform = `translateX(${index / width}%)`
-        }
-    }
 
 };
 
 const changeCardThree = () => {
-    dotOne.classList.remove('dot--active')
-    dotTwo.classList.remove('dot--active')
-    dotThree.classList.add('dot--active')
+    dotOne.classList.remove('dot--active');
+    dotTwo.classList.remove('dot--active');
+    dotThree.classList.add('dot--active');
 
-    sliderBox.style.transform = `translateX(${-width}%)`
-
-    if (window.innerWidth <= 375) {
-        let width = 102;
-
-        sliderBox.style.transform = `translateX(${-width}%)`
-        // box.style.paddingRight = '1.5rem';
-
-
-    }
+    sliderBox.style.transform = `translateX(${-width}%)`;
+    cardOne.style.transform = `translateX(${move}%)`;
+    cardOne.style.pointerEvents = "none";
+    cardThree.style.pointerEvents = "";
 
 };
 
@@ -73,42 +58,3 @@ const changeCardThree = () => {
 dotOne.addEventListener('click', changeCardOne);
 dotTwo.addEventListener('click', changeCardTwo);
 dotThree.addEventListener('click', changeCardThree);
-
-
-
-// export const changeCard = () => {
-
-//     allCards.forEach((card) => {
-
-//         card.addEventListener('click', () => {
-//             allCards.forEach(c => {
-//                 if (c.classList.contains('people__cards--active')) {
-//                     c.classList.remove('people__cards--active')
-//                 }
-//             })
-
-//             card.classList.add('people__cards--active')
-//         })
-
-//     })
-
-
-// }
-
-
-// export const activeDot = () => {
-//     alldot.forEach((dot) => {
-//         dot.addEventListener('click', () => {
-//             alldot.forEach(d => {
-//                 if (d.classList.contains('dot--active')) {
-//                     d.classList.remove('dot--active')
-//                 }
-//             })
-//             dot.classList.add('dot--active')
-//         })
-//     })
-// }
-
-
-
-// activeDot()
